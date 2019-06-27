fe3f112262656c195148773b04216178d4736423
zven1x <51484893+zven1x@users.noreply.github.com>
2019-06-27 20:24:41 +0200

    CREATING TEST METHODS" OPENING FORM1" in the UnitTest

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DBcars;
+namespace DBcars_UnitTest_1
+{
+    [TestClass]
+    public class TestForm1
+    {
+        [TestMethod]
+        public void Open_Form1_test_method()
+        {
+            Form1 form1 = new Form1();
+            form1.Show();
+        }
+    }
+}
